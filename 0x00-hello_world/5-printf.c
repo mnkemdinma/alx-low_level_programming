2641c13ff306422055d40bbf1fa8a7ad6c318021
Mbah <nkem_mbah@hotmail.com>
2022-10-15 13:19:43 -0700

    Hello printf update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
