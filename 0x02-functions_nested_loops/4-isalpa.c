63124451955a41ee425d56eefa44663c07ba808f
Mbah <nkem_mbah@hotmail.com>
2022-10-19 19:56:30 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpa.c
+++ 0x02-functions_nested_loops/4-isalpa.c
@@ -2,8 +2,9 @@
 
 /**
  * _isalpha - checks for char is alphabet
- *  @c: is the char to be checked
- *  Return: 1 if char is letter lower or upper, 0 otherwise
+ * @c: is the char to be checked
+ *
+ * Return: 1 if char is letter lower or upper, 0 otherwise
  */
 
 int _isalpha(int c)
