37aedb956fd691c5dd5facd95febf9412f1a1bc6
Mbah <nkem_mbah@hotmail.com>
2022-10-31 12:46:32 -0700

    0-main.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -28,3 +28,19 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	}
 	printf("\n");
 }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);    
+	return (0);
+}
