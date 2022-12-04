2e4f98435c3084dbeab59fb414c7755056e0556a
Mbah Nkemdinma <nkem_mbah@hotmail.com>
2022-12-04 10:29:38 -0800

    The success combination in business is: Do what you do better.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
-		
+
 /**
- * main - Prints numbers between 012 to 789.
- *
+ * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, e, g;
-				
-	i = 48;
-	e = 48;
-	g = 48;
+	int n, m, l;
 
-	while (e < 58)
+	for (n = 48; n < 58; n++)
 	{
-		i = 48;
-		while (i < 58)
+		for (m = 49; m < 58; m++)
 		{
-			g = 48;
-			while (g < 58)
+			for (l = 50; l < 58; l++)
 			{
-				if (e != i && e != g && i != g && e < i && i < g)
+				if (l > m && m > n)
 				{
-					putchar(e);
-					putchar(i);
-					putchar(g);
-					if (i == 56 && e == 55 && g == 57)
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
-						break;
+						putchar(',');
+						putchar(' ');
 					}
-					putchar(',');
-					putchar(' ');
 				}
-				g++;
 			}
-			i++;
 		}
-		e++;
 	}
 	putchar('\n');
 	return (0);
